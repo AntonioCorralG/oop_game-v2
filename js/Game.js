79f79631826ed786c9ec0d57a6306644644a4154
AntonioCorralG <97654161+AntonioCorralG@users.noreply.github.com>
2022-06-17 13:16:02 -0700

    fixed new phrase error

--- js/Game.js
+++ js/Game.js
@@ -7,14 +7,10 @@ class Game {
     this.missed = 0;
     this.phrases = [
       new Phrase("May the force be with you"),
-      ,
       new Phrase("There is no place like home"),
-      ,
       new Phrase("You are going to need a bigger boat"),
-      ,
       new Phrase("I will be back"),
-      ,
-      new Phrase("I see dead people"),
+      new Phrase("I see dead people")
     ];
     this.activePhrase = null;
   }
@@ -39,7 +35,12 @@ class Game {
   }
 
   removeLife() {
-    const missedlife = document.querySelectorAll('.tries img');
+    const missedLife = document.querySelectorAll('.tries img');
+        missedLife[this.missed].src = 'images/lostHeart.png';
+        this.missed += 1;
+        if(this.missed > 4) {
+            this.gameOver()
+        }
 
   }
 
